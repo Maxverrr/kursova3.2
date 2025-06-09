a28349c14e9f57219f0b2ee30a1b017bb879661f
maxverrr <maxbrydun@gmail.com>
2025-06-09 23:51:41 +0300

    full_done

--- frontend/src/components/CarList.jsx
+++ frontend/src/components/CarList.jsx
@@ -159,7 +159,7 @@ const CarList = () => {
                         <Link to="/cars/683ed7492979b884fda5ed4d">
                             <img src="/img/topaudi.JPEG" alt="Audi RS6" className="w-full h-auto" />
                         </Link>
-                        <div className="absolute inset-x-0 bottom-0 h-0 group-hover:h-14 bg-gradient-to-t from-black/90 to-black/70 transition-all duration-300 flex items-center justify-center">
+                        <div className="absolute inset-x-0 bottom-0 h-0 group-hover:h-14 bg-gradient-to-t from-black/90 to-black/100 transition-all duration-300 flex items-center justify-center">
                             <Link
                             to="/cars/683ed7492979b884fda5ed4d"
                             className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-150 px-6 py-2 bg-orange-900 rounded-full text-white font-medium"
@@ -174,7 +174,7 @@ const CarList = () => {
                         <Link to="/cars/683ed7492979b884fda5ed51">
                             <img src="/img/topbmw.JPEG" alt="BMW M5"/>
                         </Link>
-                        <div className="absolute inset-x-0 bottom-0 h-0 group-hover:h-14 bg-gradient-to-t from-black/90 to-black/70 transition-all duration-300 flex items-center justify-center">
+                        <div className="absolute inset-x-0 bottom-0 h-0 group-hover:h-14 bg-gradient-to-t from-black/90 to-black/100 transition-all duration-300 flex items-center justify-center">
                             <Link
                             to="/cars/683ed7492979b884fda5ed51"
                             className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-150 px-6 py-2 bg-orange-900 rounded-full text-white font-medium"
@@ -188,7 +188,7 @@ const CarList = () => {
                         <Link to="/cars/683ee2745bdbcc2301360100">
                             <img src="/img/topmerc.JPEG" alt="Mercedes-Benz C63"/>
                         </Link>
-                        <div className="absolute inset-x-0 bottom-0 h-0 group-hover:h-14 bg-gradient-to-t from-black/90 to-black/70 transition-all duration-300 flex items-center justify-center">
+                        <div className="absolute inset-x-0 bottom-0 h-0 group-hover:h-14 bg-gradient-to-t from-black/90 to-black/100 transition-all duration-300 flex items-center justify-center">
                             <Link
                             to="/cars/683ee2745bdbcc2301360100"
                             className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-150 px-6 py-2 bg-orange-900 rounded-full text-white font-medium"
